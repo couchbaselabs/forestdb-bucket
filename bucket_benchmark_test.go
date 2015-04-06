d434af580f7acef26baf733c451589aaff329907
Traun Leyden <tleyden@couchbase.com>
2015-04-06 07:58:02 -0700

    added parallel get, didn't blow up as expected it would

--- bucket_benchmark_test.go
+++ bucket_benchmark_test.go
@@ -37,3 +37,24 @@ func BenchmarkGet(b *testing.B) {
 	}
 
 }
+
+func BenchmarkParallelGet(b *testing.B) {
+
+	bucket, tempDir := GetTestBucket()
+	defer func() {
+		CloseBucket(bucket)
+		os.RemoveAll(tempDir)
+	}()
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			var value interface{}
+			err := bucket.Get("key", &value)
+			if err == nil {
+				log.Panicf("No error calling bucket.Get(), expected error")
+			}
+		}
+	})
+
+}
