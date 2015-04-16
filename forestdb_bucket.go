3a61dc51b8c15ef6bd273c1af90d072ce2e94b55
Traun Leyden <tleyden@couchbase.com>
2015-04-16 10:20:27 -0700

    fix gateload bug with not seeing auth'd users

--- forestdb_bucket.go
+++ forestdb_bucket.go
@@ -363,7 +363,9 @@ func (bucket *forestdbBucket) WriteUpdate(key string, expires int, callback walr
 		}
 		defer doc.Close()
 
+		bucket.lock.Lock()
 		err = bucket.kvstore.Get(doc)
+		bucket.lock.Unlock()
 		if err != nil && err != forestdb.RESULT_KEY_NOT_FOUND {
 			// if it's an unexpected error, return it
 			log.Printf("WriteUpdate got unexpected err calling Get(): %v", err)
